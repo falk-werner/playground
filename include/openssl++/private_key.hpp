9f8839aebc6ddee98faa6df82802bba27311233e
Falk Werner <falk.werner@gmx.net>
2019-11-24 11:15:22 +0100

    changed license to MPLv2

--- include/openssl++/private_key.hpp
+++ include/openssl++/private_key.hpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #ifndef OPENSSL_PRIVATE_KEY_HPP
 #define OPENSSL_PRIVATE_KEY_HPP
 
